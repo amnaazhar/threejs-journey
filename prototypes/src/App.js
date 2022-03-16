7416adbe082568f5f1ff04ae379ebb87b30b985c
Amna Azhar <amnaazhar94@gmail.com>
2022-03-15 23:49:29 -0400

    styles added

--- prototypes/src/App.js
+++ prototypes/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{margin: "120px auto 40px auto"}}>PROTOTYPES</h1>
+      <div class="buttons">
+      <button>Chapter 1: Basics</button>
+      <button class="button disabled">Chapter 2: Classic Techniques</button>
+      <button class="button disabled">Chapter 3: Advanced Techniques</button>
+      <button class="button disabled">Chapter 4: Shaders</button>
+      <button class="button disabled">Chapter 5: Extra</button>
+      </div>
     </div>
   );
 }
