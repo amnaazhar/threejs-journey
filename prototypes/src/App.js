9e0f0d307c1fbd9c94249d6cc78dbdc615bf74ff
Amna Azhar <amnaazhar94@gmail.com>
2022-03-16 02:57:03 -0400

    landing page

--- prototypes/src/App.js
+++ prototypes/src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{margin: "120px auto 40px auto"}}>PROTOTYPES</h1>
+      <div class="headings">
+        <h1 style={{margin: "20px auto 0px auto"}}>PROTOTYPES,</h1>
+        <h6 style={{margin: "0px 0 30px 300px"}}>I made when doing the Bruno - Three JS course</h6>
+      </div>
       <div class="buttons">
-      <button>Chapter 1: Basics</button>
-      <button class="button disabled">Chapter 2: Classic Techniques</button>
-      <button class="button disabled">Chapter 3: Advanced Techniques</button>
-      <button class="button disabled">Chapter 4: Shaders</button>
-      <button class="button disabled">Chapter 5: Extra</button>
+        <button>CHAPTER 1: Basics</button>
+        <button class="button disabled">CHAPTER 2: Classic Techniques</button>
+        <button class="button disabled">CHAPTER 3: Advanced Techniques</button>
+        <button class="button disabled">CHAPTER 4: Shaders</button>
+        <button class="button disabled">CHAPTER 5: Extra</button>
       </div>
     </div>
   );
