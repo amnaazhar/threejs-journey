fc53fb3ab8250f424198610319e4b162f286a22d
Amna Azhar <amnaazhar94@gmail.com>
2022-03-28 23:21:03 -0400

    adding chapter1-basics

---
+++ chapters/chapter1-basics/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import React, {useEffect, useRef} from "react"
+import WebGLApp from "./webglapp";
+
+function App() {
+
+  const containerRef = useRef(null); // for the canvas
+  const webglApp = useRef(null);
+
+  useEffect(()=>{
+
+    if(containerRef == null) return;
+
+    webglApp.current = new WebGLApp(containerRef.current, {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    })
+
+    function onResize () {
+      webglApp.current.resize(window.innerWidth, window.innerHeight);
+    }
+
+    window.addEventListener('resize', onResize);
+
+  },[])
+
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
