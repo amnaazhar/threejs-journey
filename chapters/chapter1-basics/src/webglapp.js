2df9ba89b1078320ebe2d1386bb32900f5273222
Amna Azhar <amnaazhar94@gmail.com>
2022-04-25 23:45:35 -0400

    pushing model

--- chapters/chapter1-basics/src/webglapp.js
+++ chapters/chapter1-basics/src/webglapp.js
@@ -5,6 +5,7 @@ import{
     Mesh,
     MeshBasicMaterial,
     Vector3,
+    PCFSoftShadowMap,
     Color,
     Group,
     BoxGeometry,
@@ -14,6 +15,15 @@ import{
     ConeGeometry,
     Clock,
     WebGLRenderer,
+    TextureLoader,
+    BufferGeometry,
+    BufferAttribute,
+    CylinderGeometry,
+    MeshPhongMaterial,
+    DoubleSide,
+    AmbientLight,
+    PointLight,
+    PlaneGeometry
 } from 'three'
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
@@ -22,19 +32,24 @@ import GUI from 'lil-gui';
 class WebGLApp {
 
     constructor(parent){
+        
         //the main function
         this.params = {
             color: 0xff0000,
-            backgroundColor: 0xc892c7
+            backgroundColor: 0xe2f0f9
         }
         const aspect = window.innerWidth / window.innerHeight
-        const clock = new Clock()
+       // const clock = new Clock()
         //set up scene
         this.scene = new Scene()
         this.scene.background = new Color(this.params.backgroundColor);
         this.camera = new PerspectiveCamera(75, aspect, 0.1, 1000)
+
         this.renderer = new WebGLRenderer({antialias:true})
         this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.renderer.shadowMap.enabled = true;
+        this.renderer.shadowMap.type = PCFSoftShadowMap;
+
         this.scene.add( this.camera )
         this.camera.position.z = 10
 
@@ -54,11 +69,27 @@ class WebGLApp {
     mousemove = (clientX, clientY) => {
         this.cursor.x = clientX / window.innerWidth - 0.5
         this.cursor.y = - (clientY / window.innerHeight - 0.5)
+        
     }
     animate = () => {
         // console.log(this.cursor.x);
-        // this.camera.position.x = this.cursor.x * 5
-        // this.camera.position.y = this.cursor.y * 5
+        this.eye_R.rotation.y = Math.sin(this.cursor.x * Math.PI * 2) 
+        this.eye_R.rotation.x = Math.cos(this.cursor.y * Math.PI * 2)
+        console.log(this.eye_R.rotation.y)
+
+        this.eye_L.rotation.y = Math.sin(this.cursor.x * Math.PI * 2) 
+        this.eye_L.rotation.x = Math.cos(this.cursor.y * Math.PI * 2) 
+
+        this.arm_L.rotation.x = Math.sin(this.cursor.x * Math.PI)
+        this.arm_R.rotation.x = -Math.sin(this.cursor.x * Math.PI) 
+       // this.arm_L.rotation.y = Math.cos(this.cursor.x * Math.PI * 2) 
+        this.leg_L.rotation.x = Math.sin(this.cursor.x * Math.PI * 0.3)
+        this.leg_R.rotation.x = -Math.sin(this.cursor.x * Math.PI * 0.3) 
+
+        this.dress.rotation.z = -Math.sin(this.cursor.x * Math.PI * 0.05) 
+        this.face.position.x = -Math.sin(this.cursor.x * Math.PI * 0.05) 
+
+        this.hair.rotation.z = -Math.sin(this.cursor.x * Math.PI * 0.05) 
 
 
         this.renderer.render( this.scene, this.camera )
@@ -73,51 +104,72 @@ class WebGLApp {
         
     }
 
+    addLights= () =>{
+        this.createAmbientLight();
+        this.createPointLight();
+    }
+
+    createAmbientLight = () => {
+        this.ambientLight = new AmbientLight(0x999999, 1);
+        this.scene.add(this.ambientLight);
+    };
+    
+    createPointLight = () => {
+        this.light = new PointLight(0xffffff, 0.3, 0);
+        this.light.castShadow = true;
+        this.light.shadow.mapSize.width = 5000;
+        this.light.shadow.mapSize.height = 5000;
+        this.light.position.set(0,20,-5);
+        this.scene.add(this.light);
+
+        const light = new PointLight(0xffffff, 0.2, 0);
+        light.castShadow = false;
+        light.position.set(0,30,50);
+        this.scene.add(light);
+    };
+
     makeScene = () => {
 
+        this.addLights()
+
         //materials
-        const skinMaterial = new MeshBasicMaterial({ color: 0xd1aa89 })
-        const dressMaterial = new MeshBasicMaterial({ color: 0xfdff7a })
-        const eyeWhite = new MeshBasicMaterial({ color: 'white' })
-        const eyeBlack = new MeshBasicMaterial({ color: 'black' })
+        // const skinMaterial = new MeshBasicMaterial({ color: 0xd1aa89 })
+        // const dressMaterial = new MeshBasicMaterial({ color: 0xfdff7a })
+        const skinMaterial = new MeshPhongMaterial({
+            color: 0xd1aa89,
+            flatShading: true,
+        });
+        const dressMaterial = new MeshPhongMaterial({
+            color: 0xfdff7a,
+            flatShading: true
+        });
+        const blackMaterial = new MeshPhongMaterial({
+            color: 0x000000,
+            flatShading: true,
+        });
+    
+        const texture = new TextureLoader().load( 'assets/textures/Vector.jpg' );
+        const eyeWhite = new MeshBasicMaterial({ map: texture })
 
         //facial
         this.face = this.makeGeometry('sphere', [1.2,32,16], skinMaterial)
         this.face.position.set(0,0.5,0)
         this.scene.add(this.face)
 
-        //eyes_white
+        //EYES - sphere
+        this.eye_L = this.makeGeometry('sphere', [0.25,32,16], eyeWhite)
+        this.eye_L.position.set(0.35, 0.5, 1.1)
+        this.eye_R = this.makeGeometry('sphere', [0.25,32,16], eyeWhite)
+        this.eye_R.position.set(-0.35, 0.5, 1.1)
+        // //eyes_white
 
-        
+        this.scene.add(this.eye_L,this.eye_R)
 
-        this.sclera_R = this.makeGeometry('circle', [0.2], eyeWhite)
-        this.sclera_R.position.set(-0.3, 0.7, 1.2)
-        this.sclera_R.rotateX(-0.22)
-
-        this.sclera_L = this.makeGeometry('circle', [0.2], eyeWhite)
-        this.sclera_L.position.set(0.3, 0.7, 1.2)
-        this.sclera_L.rotateX(-0.22)
-        //eyes_black
-        this.pupil_R = this.makeGeometry('circle', [0.1], eyeBlack)
-        this.pupil_R.position.set(-0.3, 0.6, 1.3)
-        
-
-        this.pupil_L = this.makeGeometry('circle', [0.1], eyeBlack)
-        this.pupil_L.position.set(0.3, 0.6, 1.3)
-
-        this.eye_L = new Group()
-        this.eye_L.add( this.sclera_L )
-        this.eye_L.add( this.pupil_L )
-
-        this.eye_R = new Group()
-        this.eye_R.add( this.sclera_R )
-        this.eye_R.add( this.pupil_R )
-
-        this.scene.add(this.eye_L, this.eye_R)
 
         //dress
-        this.dress = this.makeGeometry('cone', [1.5,3.5,32], dressMaterial)
+        this.dress = this.makeGeometry('cone', [1.4,2.5,32], dressMaterial)
         this.dress.position.set(0,-2,0)
+        this.dress.castShadow = true
         this.scene.add(this.dress)
 
         //arms
@@ -128,7 +180,8 @@ class WebGLApp {
         this.arm_R = this.makeGeometry('capsule', [0.2,1,10], skinMaterial)
         this.arm_R.position.set(1.5, -1.74,0)
         this.arm_R.rotation.set(-0.4, -0.2,1.7)
-        
+        this.arm_L.castShadow = true
+        this.arm_R.castShadow = true
         this.scene.add(this.arm_R, this.arm_L)
 
         //legs
@@ -137,8 +190,33 @@ class WebGLApp {
         
         this.leg_L = this.makeGeometry('capsule', [0.2,2,10], skinMaterial)
         this.leg_L.position.set(0.5, -4.5,0)
+
+        this.leg_L.castShadow = true
+        this.leg_R.castShadow = true
         
         this.scene.add(this.leg_R, this.leg_L)
+
+        //hair
+        this.hair_top = this.makeGeometry('cone', [1.6, 0.7,32], blackMaterial)
+        this.hair_top.position.set(0,1.5,0)
+       // this.scene.add(this.hair_top)
+
+
+        //hair
+        this.hair_back = this.makeGeometry('cylinder', [1.6, 2, 2.4, 32], blackMaterial)
+        this.hair_back.position.set(0,0,0)
+        this.hair_back.rotation.set(0,Math.PI/2,0)
+       // this.scene.add(this.hair)
+
+       //ground
+       const ground = new Mesh(new PlaneGeometry( 10,38 ), new MeshPhongMaterial({ //0xc1d5e1
+        color: 0xc1d5e1,
+        flatShading: true, }))
+        ground.position.set(0,-5,-8)
+        ground.rotation.set(-Math.PI/2.2,0,0)
+        ground.receiveShadow = true
+        this.scene.add( ground )
+
 
         //GUI
         //Debug Controls
@@ -151,8 +229,8 @@ class WebGLApp {
         this.makeGUI(gui, "leg_Left", this.leg_L)
         this.makeGUI(gui, "arm_Left", this.arm_L)
         this.makeGUI(gui, "arm_Right", this.arm_R)
-        this.makeGUI(gui, "pupil_R", this.pupil_R)
-        this.makeGUI(gui, "sclera_R", this.sclera_R)
+        this.makeGUI(gui, "eye_R", this.eye_R)
+        // this.makeGUI(gui, "sclera_R", this.sclera_R)
         
         //scene
         gui
@@ -163,7 +241,45 @@ class WebGLApp {
 
         gui.close()
 
+        //hair
+
+        const geometry = new BufferGeometry();
+        // create a simple square shape. We duplicate the top left and bottom right
+        // vertices because each vertex needs to appear once per triangle.
+        const vertices = new Float32Array( [
+            -2, -1.2,  1.0,
+           2, -1.2,  1.0,
+            1.55,  1.2,  1.0,
+       
+            1.55,  1.2,  1, //1.6, 2, 2.4
+           -1.55,  1.2,  1.0,
+           -2.0, -1.2,  1.0
+        ] );
+
+        // itemSize = 3 because there are 3 values (components) per vertex
+        geometry.setAttribute( 'position', new BufferAttribute( vertices, 3 ) );
+        const hair_front = new Mesh( geometry, blackMaterial);
+        hair_front.position.set(0,-0,-1.1)
+      //  this.scene.add(hair_front)
+        // this.hair_back.castShadow = true
+        // this.hair_top.castShadow = true
+        // hair_front.castShadow = true
+
+        this.hair = new Group()
+        this.hair.add( this.hair_back, this.hair_top, hair_front )
+
+        this.scene.add(this.hair)
+
+
     }
+
+    makeClouds = () => {
+
+
+      //  const cloud_1 = this.makeGeometry('sphere', [0.25,32,16], eyeWhite))
+        
+    }
+
 
     makeGUI = (gui, folderName, obj) => {
         //gui face
@@ -246,6 +362,12 @@ class WebGLApp {
             case "circle":
                 geometry = new Mesh(
                     new CircleGeometry(params[0], 32), // radius segs
+                    material
+                );
+                break
+            case "cylinder":
+                geometry = new Mesh(
+                    new CylinderGeometry(params[0],params[1], params[2], params[3], 1, false, 0, Math.PI), //radiusTop : Float, radiusBottom : Float, height : Float, radialSegments
                     material
                 );
                 break
